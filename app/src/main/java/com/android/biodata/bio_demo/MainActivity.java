7df40a7db6404c9bc8a7224ffe3b6c86bb9235a5
Sai Sreenivas <saisreenivas222@gmail.com>
2020-07-18 12:59:34 +0530

    first commit

---
+++ app/src/main/java/com/android/biodata/bio_demo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.android.biodata.bio_demo;
+
+import android.content.ContentValues;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/android/biodata/bio_demo/SQLiteDatabaseHelper.java
@@ -0,0 +1,25 @@
+package com.android.biodata.bio_demo;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+class SQLiteDatabaseHelper extends SQLiteOpenHelper{
+
+    public SQLiteDatabaseHelper(Context context, String databaseName,int databaseVersion ) {
+        super(context, databaseName, null, databaseVersion );
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS ");
+        onCreate(db);
+    }
+
+}
